f4ceff678ca0dde8f215cdfebc01badd4c368336
cynthiagomezmontoya <cgomezm066@gmail.com>
2020-01-23 22:54:49 -0400

    inicializando firebase

--- construred/src/index.js
+++ construred/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import firebase from 'firebase';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+firebase.initializeApp({
+apiKey: "AIzaSyDRj__4Ty1hwTj3rg1aTltsE1OjC6z2ckk",
+    authDomain: "construred-d2c22.firebaseapp.com",
+    databaseURL: "https://construred-d2c22.firebaseio.com",
+    storageBucket: "construred-d2c22.appspot.com",
+    messagingSenderId: "145471676980",
+    
+  });
+
+ReactDOM.render(
+<App />, 
+document.getElementById('root')
+);
+
